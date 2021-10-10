c1100d3f4d8f3ae69f6227c0778e467d2b32b662
alexey astreika <astrolex3003@gmail.com>
2021-10-10 17:10:33 +0300

    stable

---
+++ parser/main.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+URL = "https://cars.av.by/filter?brands[0][brand]=683"
+HEADERS = {
+    'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0',
+    'accept': '*/*',
+}
+HOST = 'https://cars.av.by'
+FILE = 'cars.csv'
+
+def get_html(url, params=None):
+    r = requests.get(url, headers= HEADERS, params=params)
+    return r
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find('h3', class_='listing__title').get_text()
+    if len(pagination)==22:
+        a= pagination[8:11]
+    elif len(pagination)==23:
+        a = pagination[8:12]
+    page= int(a)//25
+    return page
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='listing-item')
+
+    cars=[]
+
+    for item in items:
+        cars.append({
+            'title': item.find('h3', class_='listing-item__title').get_text(),
+            'link': HOST + item.find('a', class_='listing-item__link').get('href'),
+            'price': item.find('div', class_='listing-item__price').get_text(),
+            'location': item.find('div', class_='listing-item__location').get_text()
+        })
+    return (cars)
+
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Марка', 'Ссылка', 'Цена', 'Город'])
+        for item in items:
+            writer.writerow([item['title'], item['link'], item['price'], item['location']])
+
+
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        cars = []
+        page_count = get_pages_count(html.text)
+        for page in range(1, page_count + 1):
+            print(f'Обработка страницы {page} из {page_count}')
+            html = get_html(URL, params={'&page=': page})
+            cars.extend(get_content(html.text))
+        save_file(cars, FILE)
+    else:
+        print('Error')
+
+parse()
+
